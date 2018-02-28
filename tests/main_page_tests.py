1311b35ef419bd0705c994ca2ee7e3ed0a6f2f8d
Rok Povšič <rok.povsic@gmail.com>
2018-02-28 10:58:22 +0100

    Dodaten test. Importajmo route iz main.py.

--- tests/main_page_tests.py
+++ tests/main_page_tests.py
@@ -6,21 +6,18 @@ import webtest
 from google.appengine.ext import testbed
 from main import MainHandler
 
+from main import app
+
 
 class MainPageTests(unittest.TestCase):
     def setUp(self):
-        app = webapp2.WSGIApplication(
-            [
-                webapp2.Route('/', MainHandler, name="main-page"),
-            ])
-
         self.testapp = webtest.TestApp(app)
         self.testbed = testbed.Testbed()
         self.testbed.activate()
 
         """ Uncomment the stubs that you need to run tests. """
         self.testbed.init_datastore_v3_stub()
-        # self.testbed.init_memcache_stub()
+        self.testbed.init_memcache_stub()
         # self.testbed.init_mail_stub()
         # self.testbed.init_taskqueue_stub()
         self.testbed.init_user_stub()
@@ -35,4 +32,8 @@ class MainPageTests(unittest.TestCase):
 
     def test_main_page_handler(self):
         get = self.testapp.get('/')  # get main handler
+        self.assertEqual(get.status_int, 200)  # if GET request was ok, it should return 200 status code
+
+    def test_moji_komentarji(self):
+        get = self.testapp.get('/moji-komentarji')  # get main handler
         self.assertEqual(get.status_int, 200)  # if GET request was ok, it should return 200 status code
